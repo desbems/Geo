d78ed0b17288fa1a998015ee0429413733757064
desbems <pootismansi@gmail.com>
2021-02-03 22:16:14 +0100

    password 0.2
    Now stores the location and leftovers in a new ligne
    Now after the first launch you won't have to reopen to use the file

--- main.py
+++ main.py
@@ -2,35 +2,54 @@ import sys
 import os
 
 # initialzing
-if os.path.isfile('./data.txt'):
-    f = open('data.txt')
-    login = f.read()
-    trypass = ""
-    location = ""
-    leftovers = ""
-    changepass = ""
-# aks the user the secret phrase to access if it's the first time skip this step
-    while (trypass != login) :
-        trypass = input("Enter your password : ")
-        if (trypass != login):
-            print("Incorrect !")
-        else :
-            print("Correct !")
-#
-    location = input("What is the location ? : ")
-    leftovers = input("Is there any leftovers ? : ")
+correct = False
+loop = 0
+while (loop != 2):
+    if os.path.isfile('./data.txt'):
+        f = open('data.txt', 'r')
+        login = f.read()
+        trypass = ""
+        location = ""
+        leftovers = ""
+        changepass = ""
 
-    os.system('cls')
-    while changepass != "Yes" or changepass != "No":
-        changepass = input("Do you want to change your password ? : (Yes/No) ")
-        if changepass == "Yes":
-            password = input("Type your new password : ")
-            sys.exit()
-        elif changepass == "No":
-            sys.exit()
-else :
-    login = input("pswrd")
-    f = open('data.txt', 'w+')
-    f.write(login)
-    f.close()
+    # aks the user the secret phrase to access if it's the first time skip this step
+        while (trypass != login) :
+            trypass = input("Enter your password :\n")
+
+
+            if (trypass != login):
+                print("Incorrect !")
+            else :
+                print("Correct !")
+        f.close()
+        location = input("What is the location ? : ")
+        f = open('data.txt', 'a')
+        f.write("\n")
+        f.write(location)
+        f.write("\n")
+        leftovers = input("\nIs there any leftovers ? : ")
+        f.write(leftovers)
+        f.write("\n")
+        f.close()
+
+
+
+        os.system('cls')
+        while changepass != "Yes" or changepass != "No":
+            changepass = input("Do you want to change your password ? : (Yes/No) ")
+            if changepass == "Yes":
+                password = input("Type your new password : ")
+                sys.exit()
+            elif changepass == "No":
+                loop = 2
+                sys.exit()
+
+    else :
+        login = input("pswrd")
+        f = open('data.txt', 'a')
+        f.write(login)
+
+        f.close()
+
 
