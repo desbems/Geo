3e352ff0907a8818a3c549edf40b5adcb6525130
desbems <pootismansi@gmail.com>
2021-02-03 20:20:46 +0100

    Added password 0.1
    Added a version where it first create a txt file with a password you have chosen and then when you relaunch it asks for the password for the program to execute as expected

--- main.py
+++ main.py
@@ -2,27 +2,35 @@ import sys
 import os
 
 # initialzing
-password = "123"
-trypass = ""
-location = ""
-leftovers = ""
-changepass = ""
+if os.path.isfile('./data.txt'):
+    f = open('data.txt')
+    login = f.read()
+    trypass = ""
+    location = ""
+    leftovers = ""
+    changepass = ""
 # aks the user the secret phrase to access if it's the first time skip this step
-while (trypass != password) :
-    trypass = input("Enter your password : ")
-    if (trypass != password):
-        print("Incorrect !")
-    else :
-        print("Correct !")
+    while (trypass != login) :
+        trypass = input("Enter your password : ")
+        if (trypass != login):
+            print("Incorrect !")
+        else :
+            print("Correct !")
 #
-location = input("What is the location ? : ")
-leftovers = input("Is there any leftovers ? : ")
+    location = input("What is the location ? : ")
+    leftovers = input("Is there any leftovers ? : ")
 
-os.system('cls')
-while changepass != "Yes" or changepass != "No":
-    changepass = input("Do you want to change your password ? : (Yes/No) ")
-    if changepass == "Yes":
-        password = input("Type your new password : ")
-        sys.exit()
-    elif changepass == "No":
-        sys.exit()
+    os.system('cls')
+    while changepass != "Yes" or changepass != "No":
+        changepass = input("Do you want to change your password ? : (Yes/No) ")
+        if changepass == "Yes":
+            password = input("Type your new password : ")
+            sys.exit()
+        elif changepass == "No":
+            sys.exit()
+else :
+    login = input("pswrd")
+    f = open('data.txt', 'w+')
+    f.write(login)
+    f.close()
+
