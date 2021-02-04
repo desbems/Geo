ac915d811bdf897bbe092c855537c0c2453c672c
desbems <pootismansi@gmail.com>
2021-02-04 18:14:40 +0100

    small fix

--- main.py
+++ main.py
@@ -1,33 +1,44 @@
 import sys
 import os
+import bcrypt
 
-def changeLoc(location, leftovers):
-        lo = open('misc.txt', 'a')
+
+
+def changeloc(location, leftovers):
+        lo = open('misc.txt', 'w')
         lo.write(location)
         lo.write("\n")
         lo.write(leftovers)
         lo.write("\n")
         lo.close()
+def passwordGen(password, salt):
+    f = open('data.txt', 'wb')
+    hashed_password = bcrypt.hashpw(password, salt)
+    f.write(hashed_password)
+    f.close()
 
-# initialzing
+# initializing
+
 correct = False
 loop = 0
-while (loop != 2):
+while loop != 2:
     if os.path.isfile('./data.txt'):
-        f = open('data.txt', 'r')
+        f = open('data.txt', 'r+')
         login = f.read()
         trypass = ""
         location = ""
         leftovers = ""
         changepass = ""
 
-    # aks the user the secret phrase to access if it's the first time skip this step
-        while (trypass != login) :
+# loop until password is not a match
+        valid = False
+        while valid != True:
             trypass = input("Enter your password : ")
-            if (trypass != login):
+            valid = bcrypt.checkpw(trypass.encode('utf8'), login.encode('utf8'))
+            if valid != True:
                 print("Incorrect !")
 
-            else :
+            else:
                 print("Correct !")
                 os.system('cls')
                 lo = open('misc.txt', 'r')
@@ -38,25 +49,24 @@ while (loop != 2):
                 if answer == "yes":
                     location = input("What is the location ? : ")
                     leftovers = input("\nIs there any leftovers ? : ")
-                    changeLoc(location, leftovers)
+                    changeloc(location, leftovers)
         os.system('cls')
-        while changepass != "Yes" or changepass != "No":
+        while changepass != "Yes" or changepass != "No" or changepass != "yes" or changepass != "no":
             changepass = input("Do you want to change your password ? : (Yes/No) ")
-            if changepass == "Yes":
-                password = input("Type your new password : ")
-                f = f = open('data.txt', 'w')
-                f.write(password)
+            if changepass == "Yes" or changepass == "yes":
+                password = input("pswrd").encode('utf8')
+                salt = bcrypt.gensalt()
+                passwordGen(password, salt)
                 sys.exit()
-            elif changepass == "No":
+            elif changepass == "No" or changepass == "no":
                 loop = 2
                 sys.exit()
-
+# aks the user the secret phrase to access if it's the first time skip this step
     else:
-        login = input("pswrd")
-        f = open('data.txt', 'w')
-        f.write(login)
-        f.close()
+        password = input("pswrd").encode('utf8')
+        salt = bcrypt.gensalt()
+        passwordGen(password, salt)
         location = input("What is the location ? : ")
         leftovers = input("\nIs there any leftovers ? : ")
-        changeLoc(location, leftovers)
+        changeloc(location, leftovers)
 
