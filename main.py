6c26e35e16d5a8dcee0e5894b2c15ed3aa3a20fa
desbems <pootismansi@gmail.com>
2021-02-16 18:46:59 +0100

    Improved database and edition of data

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ import sqlite3
 connection = sqlite3.connect('location.db')
 cursor = connection.cursor()
 # create tables
-command1 = """CREATE TABLE IF NOT EXISTS locations(location_id, real_loc TEXT, leftovers TEXT)"""
+command1 = """CREATE TABLE IF NOT EXISTS locations(id integer primary key, real_loc TEXT, leftovers TEXT)"""
 cursor.execute(command1)
-n = 1
+select_id = 0
+
 def printLoc():
         cursor.execute("SELECT * FROM locations")
-        result = cursor.fetchall()
-        print(result)
+        [print(result) for result in cursor.fetchall()]
         print("id, location, still there")
 def passwordgen(password, salt):
     f = open('data.txt', 'wb')
@@ -50,24 +50,35 @@ while loop != 2:
                     answer2 = 0
                     while answer2 > 3 or answer2 < 1:
                         print("Change location ..........(1)")
-                        print("Change password ..........(2)")
-                        print("Exit......................(3)")
+                        print("Add a new location .......(2)")
+                        print("Change password ..........(3)")
+                        print("Exit......................(4)")
                         answer2 = int(input())
                     if answer2 == 1:
                         system('cls')
-                        location = input("What is the location ? : ")
-                        leftovers = input("\nIs it still there ? : ")
-                        system('cls')
                         printLoc()
-                        n = int(input("Enter the number of the line you want to change, enter a new one if you want to create a new one"))
-                        cursor.execute("insert into locations (location_id, realoc, leftovers) values (?, ?, ?)", (n, location, leftovers))
+                        row = int(input("what row you want to change ?"))
+                        location = input("What is the location ? : ")
+                        leftovers = input("Is it still there ? : ")
+                        system('cls')
+                        cursor.execute("UPDATE locations SET real_loc = (?) WHERE id = (?)", (location, row))
+                        cursor.execute("UPDATE locations SET leftovers = (?) WHERE id = (?)", (leftovers, row))
+                        printLoc()
                         connection.commit()
                     elif answer2 == 2:
+                        system('cls')
+                        printLoc()
+                        location = input("What is the location ? : ")
+                        leftovers = input("Is it still there ? : ")
+                        cursor.execute("insert into locations (real_loc, leftovers) values (?, ?)", (location, leftovers))
+                        connection.commit()
+                        system('cls')
+                    elif answer2 == 3:
                         password = input("Enter your new password : ").encode('utf8')
                         salt = bcrypt.gensalt()
                         passwordgen(password, salt)
-
-                    elif answer2 ==3:
+                        system('cls')
+                    elif answer2 ==4:
                         sys.exit()
                 else:
                     sys.exit()
@@ -78,5 +89,6 @@ while loop != 2:
         passwordgen(password, salt)
         location = input("What is the location ? : ")
         leftovers = input("Is it still there ? : ")
-        cursor.execute("insert into locations (location_id, real_loc, leftovers) values (?, ?, ?)", (n, location, leftovers))
+        cursor.execute("insert into locations (real_loc, leftovers) values (?, ?)", (location, leftovers))
         connection.commit()
+        system('cls')
