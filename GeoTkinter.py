7758a60060aaa3067eacbb30b2144a19b929cac9
desbems <pootismansi@gmail.com>
2021-02-20 16:13:46 +0100

    Initial build
    First password interface

--- GeoTkinter.py
+++ GeoTkinter.py
@@ -1,7 +1,33 @@
 from tkinter import *
+import bcrypt
 
-# Window
+# Initialisation
 root = Tk()
+root.title("GeoProject")
+root.geometry("400x300")
+password = "123"
+gpass = Label(root, text="Good password")
+bpass = Label(root, text="Bad password")
+#
+def passCheck():
+	trypass = inputPassword.get()
+	if trypass == password:
+		gpass.pack()
+		btnOk.place_forget()
+		inputPassword.place_forget()
+
+	else:
+		bpass.pack()
+
+
+
+# Phase 1 : Password Checking / Creation
+inputPassword = Entry(root)
+inputPassword.place(x=0, y=0)
+btnOk = Button(root, text="Enter your password", command=passCheck)
+btnOk.place(x=0, y=30)
+
+
 
 # print Window
 root.mainloop()
